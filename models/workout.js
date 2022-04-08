233cb6766dad54c861a7508d16cc719bb698a41a
jenniferdinh1997 <jenniferdinh1997@gmail.com>
2022-04-08 11:41:42 -0700

    finish schemas

--- models/workout.js
+++ models/workout.js
@@ -1,8 +1,39 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema ({
-    
+const exerciseSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    bodyPart: {
+        type: String,
+        enum: ['Back', 'Biceps', 'Triceps', 'Shoulders', 'Chest', 'Legs', 'Cardio']
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: true
+    }
 })
 
+const workoutSchema = new Schema ({
+    date: {
+        type: Date,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    exercise: [exerciseSchema],
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5
+    }
+})
 module.exports = mongoose.model('Workout', workoutSchema);
