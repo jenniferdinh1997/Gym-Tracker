6a97cadff02641b5e4369bb0b55ee0aa0582d968
jenniferdinh1997 <jenniferdinh1997@gmail.com>
2022-04-13 11:12:31 -0700

    fix passport

--- config/passport.js
+++ config/passport.js
@@ -9,16 +9,32 @@ passport.use(new GoogleStrategy({
     callbackURL: process.env.GOOGLE_CALLBACK
   },
   function(accessToken, refreshToken, profile, cb) {
-
-
+    User.findOne({googleId: profile.id}).then(async function(user) {
+      if(user) return cb(null,user);
+      try {
+        user = await User.create({
+          name: profile.displayName,
+          googleId: profile.id,
+          email: profile.emails[0].value,
+          avatar: profile.photos[0].value
+        });
+        return cb(null,user);
+      } catch(err) {
+        return cb(err);
+      }
+    })
   }
 ));
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
+passport.serializeUser(function(user, cb) {
+  cb(null, user._id);
 });
 
-passport.deserializeUser(function(id, done) {
+passport.deserializeUser(function(userId, cb) {
+  User.findById(userId, function(err,user) {
+    if(err) return cb(err);
+    cb(null,user);
+  })
 
   // Find your User, using your model, and then call done(err, whateverYourUserIsCalled)
   // When you call this done function passport assigns the user document to req.user, which will 
