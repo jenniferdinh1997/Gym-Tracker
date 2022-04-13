8769523633324305ee47bfe72ba8e722ea6bcc64
jenniferdinh1997 <jenniferdinh1997@gmail.com>
2022-04-12 23:20:02 -0700

    fix date

--- controllers/workout.js
+++ controllers/workout.js
@@ -18,6 +18,8 @@ function index(req,res) {
 
 function newWorkout(req,res) {
     const workout = new Workout(req.body);
+    const d = req.body.date;
+    req.body.date = `${d.substr(5, 2)}-${d.substr(8, 2)}-${d.substr(0, 4)}`;
     workout.save(function(err) {
         res.redirect('/workout/finish');
     })
